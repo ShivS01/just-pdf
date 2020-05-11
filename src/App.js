f2f99df0269d6273977766933d115e9534316035
ShivS01 <43378694+ShivS01@users.noreply.github.com>
2020-05-11 19:23:39 +0530

    Laying out basic components and routing structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./Home";
+import Books from "./Books";
+import About from "./About";
+import Contact from "./Contact";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/books" exact component={Books} />
+          <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <h3>Logo</h3>
+      <ul className="nav-links">
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/books">
+          <li>Books</li>
+        </Link>
+        <Link to="/about">
+          <li>About</li>
+        </Link>
+        <Link to="/contact">
+          <li>Contact</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
