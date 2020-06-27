103460d7489ff2b6bfe20ba1f96c233058f65a3f
ShivS01 <43378694+ShivS01@users.noreply.github.com>
2020-06-28 04:08:22 +0530

    changes to test route

--- src/components/Book.js
+++ src/components/Book.js
@@ -55,55 +55,60 @@ const useStyles = makeStyles((theme) => ({
 const Book = () => {
   const classes = useStyles();
   return (
-    <Card className={classes.root} elevation={10}>
-      <CardHeader title="Atomic Habits" subheader="James Clear" />
-      <CardActionArea className={classes.align}>
-        <CardMedia
-          className={classes.media}
-          image={imagee}
-          title="Book Cover"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h6" component="h2">
-            THE PHENOMENAL INTERNATIONAL BESTSELLER – 1 MILLION COPIES SOLD
-          </Typography>
-          <Typography
-            variant="body2"
-            color="textPrimary"
-            component="p"
-            gutterBottom
-          >
-            Transform your life with tiny changes in behaviour – starting
-            now.People think that when you want to change your life, you need to
-            think big. But world-renowned habits expert James Clear has
-            discovered another way.
-          </Typography>
-          <Typography variant="subtitle2" color="textSecondary" component="p">
-            Size : 30MB
-          </Typography>
-          <Typography variant="subtitle2" color="textSecondary" component="p">
-            Pages : 300
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <IconButton aria-label="download">
-          <GetAppIcon />
-        </IconButton>
-        <IconButton aria-label="amazon">
-          <img src={amazon} className={classes.icon} alt="amazon" />
-        </IconButton>
-        <IconButton aria-label="flipkart">
-          <img src={flipkart} className={classes.icon} alt="flipkart" />
-        </IconButton>
-      </CardActions>
-    </Card>
+    <>
+      <Typography align="center" style={{ margin: "20px" }}>
+        Test Branch for testing book widgets/cards
+      </Typography>
+      <Card className={classes.root} elevation={10}>
+        <CardHeader title="Atomic Habits" subheader="James Clear" />
+        <CardActionArea className={classes.align}>
+          <CardMedia
+            className={classes.media}
+            image={imagee}
+            title="Book Cover"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="h2">
+              THE PHENOMENAL INTERNATIONAL BESTSELLER – 1 MILLION COPIES SOLD
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textPrimary"
+              component="p"
+              gutterBottom
+            >
+              Transform your life with tiny changes in behaviour – starting
+              now.People think that when you want to change your life, you need
+              to think big. But world-renowned habits expert James Clear has
+              discovered another way.
+            </Typography>
+            <Typography variant="subtitle2" color="textSecondary" component="p">
+              Size : 30MB
+            </Typography>
+            <Typography variant="subtitle2" color="textSecondary" component="p">
+              Pages : 300
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <IconButton aria-label="download">
+            <GetAppIcon />
+          </IconButton>
+          <IconButton aria-label="amazon">
+            <img src={amazon} className={classes.icon} alt="amazon" />
+          </IconButton>
+          <IconButton aria-label="flipkart">
+            <img src={flipkart} className={classes.icon} alt="flipkart" />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </>
   );
 };
 
